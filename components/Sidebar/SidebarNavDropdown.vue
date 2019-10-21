73416f32d9977525481366b8be608a77318af248
kawamoto <bahihzss@gmail.com>
2019-10-21 16:28:37 +0900

    新規登録のクリックイベントが発火しないバグを修正

--- components/Sidebar/SidebarNavDropdown.vue
+++ components/Sidebar/SidebarNavDropdown.vue
@@ -1,32 +1,30 @@
 <template>
-  <router-link tag="li" class="nav-item nav-dropdown" :to="url" disabled>
-    <div class="nav-link nav-dropdown-toggle" @click="handleClick"><i :class="icon"></i> {{name}}</div>
+  <li class="nav-item nav-dropdown" :class="{ open }">
+    <div class="nav-link nav-dropdown-toggle" @click.prevent="open=!open">
+      <i :class="icon"></i> {{ name }}
+    </div>
     <ul class="nav-dropdown-items">
-      <slot></slot>
+      <slot/>
     </ul>
-  </router-link>
+  </li>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      open: false
+    }
+  },
+
   props: {
     name: {
-      type: String,
-      default: ''
-    },
-    url: {
       type: String,
       default: ''
     },
     icon: {
       type: String,
       default: ''
-    }
-  },
-  methods: {
-    handleClick (e) {
-      e.preventDefault()
-      e.target.parentElement.classList.toggle('open')
     }
   }
 }
--- components/Sidebar/SidebarProduct.vue
+++ components/Sidebar/SidebarProduct.vue
@@ -1,111 +1,117 @@
 <template>
   <div>
-  <ul class="nav sidebar-product">
-    <li class="nav-title">ブランド</li>
-
-    <template v-for="(item) in brands">
-      <SidebarNavDropdown :name="item.name" icon="icon-tag" v-bind:key="item.id">
-        <template v-for="(product, index) in products[item.id]">
-          <li class="nav-item" v-bind:key="index">
-            <nuxt-link class="nav-link" v-bind:to="{name:'product-id-lots',params: {id: product.id}}">
-              <span class="jan-code">JAN : {{ product.jan_code}}</span>
+    <ul class="nav sidebar-product">
+      <li class="nav-title">ブランド</li>
+      <SidebarNavDropdown
+        v-for="brand in brands"
+        :name="brand.name"
+        icon="icon-tag"
+        :key="'brand-' + brand.id">
+        <template v-if="products.hasOwnProperty(brand.id)">
+          <li
+            class="nav-item"
+            v-for="product in products[brand.id]"
+            :key="'product-' + product.id">
+            <nuxt-link
+              class="nav-link"
+              :to="{ name:'product-id-lots', params: { id: product.id } }">
+              <span class="jan-code">JAN : {{ product.jan_code }}</span>
               <div class="name">
                 <i class="nav-icon icon-puzzle"></i>
-                <span>{{ product.current_lot ? product.current_lot.name : 'lotがありません'}}</span>
+                <span>
+                  {{ product.current_lot ? product.current_lot.name : 'lotがありません'}}
+                </span>
               </div>
             </nuxt-link>
           </li>
-          <li class="nav-item nav-control" v-bind:key="'button-' + index"
-              v-if="products[item.id].length === index + 1">
-            <a @click="showModal('product', item.id)">
-              新規登録
-            </a>
-          </li>
         </template>
-        <template v-if="products[item.id] === undefined">
-          <li class="nav-item" v-bind:key="0">
+        <template v-else>
+          <li class="nav-item">
             <a class="nav-link">
               <span class="jan-code">登録されていません</span>
             </a>
           </li>
-          <li class="nav-item nav-control" v-bind:key="'button-' + 0"
-          >
-            <a @click="showModal('product', item.id)">
-              新規登録
-            </a>
-          </li>
         </template>
+        <li class="nav-item nav-control">
+          <a @click.prevent.stop="showModal('product', brand.id)">
+            新規登録
+          </a>
+        </li>
       </SidebarNavDropdown>
-    </template>
-    <li class="nav-control">
-      <a @click="showModal('brand')">
-        新規登録
-      </a>
-    </li>
-  </ul>
-  <BrandModal v-if="showModalBrand" @close="closeModal('brand')" />
-  <ProductModal v-if="showModalProduct" @close="closeModal('product')" v-bind:brand_id="selectedBrandId" />
+      <li class="nav-control">
+        <a @click.prevent="showModal('brand')">
+          新規登録
+        </a>
+      </li>
+    </ul>
+    <BrandModal
+      v-if="showModalBrand"
+      @close="closeModal('brand')"/>
+    <ProductModal
+      v-if="showModalProduct"
+      @close="closeModal('product')"
+      :brand_id="selectedBrandId"/>
   </div>
 </template>
 
 <script>
-  import BrandModal from '../Modal/BrandModal'
-  import ProductModal from '../Modal/ProductModal'
-  import SidebarNavDropdown from './SidebarNavDropdown'
+import BrandModal from '../Modal/BrandModal'
+import ProductModal from '../Modal/ProductModal'
+import SidebarNavDropdown from './SidebarNavDropdown'
 
-  export default {
-    name: 'sidebar-product',
-    components: {
-      SidebarNavDropdown,
-      BrandModal,
-      ProductModal,
+export default {
+  name: 'sidebar-product',
+  components: {
+    SidebarNavDropdown,
+    BrandModal,
+    ProductModal,
+  },
+  data () {
+    return {
+      formData: {
+        id: this.$store.$auth.user.id,
+      },
+      showModalBrand: false,
+      showModalProduct: false,
+      selectedBrandId: false,
+    }
+  },
+  props: {
+    brands: {
+      type: Array,
+      default: () => []
     },
-    data () {
-      return {
-        formData: {
-          id: this.$store.$auth.user.id,
-        },
-        showModalBrand: false,
-        showModalProduct: false,
-        selectedBrandId: false,
+    products: {
+      type: Array | Object,
+      default: () => []
+    },
+  },
+  methods: {
+    showModal (type, brand_id = null) {
+      switch (type) {
+        case 'brand':
+          this.showModalBrand = true
+          break
+        case 'product':
+          this.selectedBrandId = brand_id
+          this.showModalProduct = true
+          break
+        default:
+          break
       }
     },
-    props: {
-      brands: {
-        type: Array,
-        default: () => []
-      },
-      products: {
-        type: Array | Object,
-        default: () => []
-      },
+    closeModal (type) {
+      switch (type) {
+        case 'brand':
+          this.showModalBrand = false
+          break
+        case 'product':
+          this.showModalProduct = false
+          break
+        default:
+          break
+      }
     },
-    methods: {
-      showModal(type, brand_id = null) {
-        switch (type) {
-          case 'brand':
-            this.showModalBrand = true;
-            break;
-          case 'product':
-            this.showModalProduct = true;
-            this.selectedBrandId = brand_id;
-            break;
-          default:
-            break;
-        }
-      },
-      closeModal(type) {
-        switch (type) {
-          case 'brand':
-            this.showModalBrand = false;
-            break;
-          case 'product':
-            this.showModalProduct = false;
-            break;
-          default:
-            break;
-        }
-      },
-    }
   }
+}
 </script>
