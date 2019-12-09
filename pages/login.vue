01be842de58c057886f1a1f485fe3be3a5905997
Seiko Tsubono <tsubono@wss.systems>
2019-12-09 17:09:01 +0900

    ログイン画面修正【1h】

--- pages/login.vue
+++ pages/login.vue
@@ -3,18 +3,20 @@
     <div>
       <b-alert v-if="alertMessage" show variant="danger">{{ alertMessage }}</b-alert>
     </div>
-    <b-row class="justify-content-center mt-5">
-      <b-col md="5">
-        <b-form-group>
-          <b-form-input type="text" id="email" placeholder="email" v-model="user.email" class="form-control"></b-form-input>
-        </b-form-group>
-        <b-form-group>
-          <b-form-input type="text" id="password" placeholder="password" v-model="user.password" class="form-control"></b-form-input>
-        </b-form-group>
-        <b-button type="submit" variant="primary" @click="passwordGrantLogin()" class="btn-block">Login</b-button>
-        <a class="pull-right" href="/register">新規登録</a>
-      </b-col>
-    </b-row>
+    <form @submit.prevent="passwordGrantLogin()">
+      <b-row class="justify-content-center mt-5">
+        <b-col md="5">
+          <b-form-group>
+            <b-form-input type="text" id="email" placeholder="email" v-model="user.email" class="form-control"></b-form-input>
+          </b-form-group>
+          <b-form-group>
+            <b-form-input type="password" id="password" placeholder="password" v-model="user.password" class="form-control"></b-form-input>
+          </b-form-group>
+          <b-button type="submit" variant="primary" class="btn-block">Login</b-button>
+          <a class="pull-right" href="/register">新規登録</a>
+        </b-col>
+      </b-row>
+    </form>
   </div>
 </template>
 
